5289d057ad08c406bcf4926ce5da6ab8f78b40e6
nsobczak <nicolas.sobczak@isen.yncrea.fr>
2016-11-02 13:54:50 +0100

    Add UserProfil

--- Modele/Modele.php
+++ Modele/Modele.php
@@ -3,9 +3,44 @@
 /**
  * Created by PhpStorm.
  * User: Nicolas Sobczak & Vincent Reynaert
- * Date: 30/10/2016
+ * Date: 02/11/2016
  */
 abstract class Modele
 {
 
+    /** Objet PDO d'accès à la BD */
+    private $bdd;
+
+    /**
+     * Renvoie un objet de connexion à la BD en initialisant la connexion au besoin
+     *
+     * @return PDO, objet PDO de connexion à la BDD
+     */
+    private function getBdd()
+    {
+        if ($this->bdd == null) {
+            // Création de la connexion
+            $this->bdd = new PDO('mysql:host=localhost; dbname=db_e_shopping; charset=utf8', 'root', 'ISEN');
+        }
+        return $this->bdd;
+    }
+
+    /**
+     * Exécute une requête SQL éventuellement paramétrée
+     *
+     * @param string $sql La requête SQL
+     * @param array $valeurs Les valeurs associées à la requête
+     * @return PDOStatement Le résultat renvoyé par la requête
+     */
+    protected function executerRequete($sql, $params = null)
+    {
+
+        if ($params == null) {
+            $resultat = $this->getBdd()->query($sql); // exécution directe
+        } else {
+            $resultat = $this->getBdd()->prepare($sql);  // requête préparée
+            $resultat->execute($params);
+        }
+        return $resultat;
+    }
 }
---
+++ Modele/UserProfil.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'Modele/Modele.php';
+
+/**
+ * Created by PhpStorm.
+ * User: Nicolas Sobczak & Vincent Reynaert
+ * Date: 02/11/2016
+ */
+class UserProfil extends Modele
+{
+
+    /** Renvoie les informations sur un utillisateurs
+     *
+     * @param int $id L'identifiant de l'utilisateur
+     * @return array L'utilisateur
+     * @throws Exception Si l'identifiant de l'utilisateur est inconnu
+     */
+    public function getUser($userID)
+    {
+        $sql = 'select userID as id, nom, prenom, niveau_accreditation, mail, mot_de_passe from user where userID=?';
+        $user = $this->executerRequete($sql, array($userID));
+        if ($user->rowCount() == 1)
+            return $user->fetch();  // Accès à la première ligne de résultat
+        else
+            throw new Exception("Aucun utilisateur ne correspond à l'identifiant '$userID'");
+    }
+}
