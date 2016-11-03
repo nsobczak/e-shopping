62a7bd1a66d98e547513b61c758b4bc7aed2d71e
VincentReynaert <vincent.reynaert@isen.yncrea.fr>
2016-11-03 18:19:09 +0100

    modif des liens dans le HTMLheader pour regler la couille

--- Vue/header.php
+++ Vue/header.php
@@ -2,16 +2,16 @@
     <a href="index.php?action=accueil"><h1 id="titreSite">GeekProducts.com</h1></a>
 
     <ul>
-        <li><a href="Controleur/Routeur.php?action=recherche">Search</a></li>
+        <li><a href="index.php?action=recherche">Search</a></li>
         <li class="dropdown">
             <a href="#" class="dropbtn">Products</a>
             <div class="dropdown-content">
-                <a href="Controleur/Routeur.php?action=product&id=1">Product 1</a>
-                <a href="Controleur/Routeur.php?action=product&id=2">Product 2</a>
+                <a href="index.php?action=product&id=1">Product 1</a>
+                <a href="index.php?action=product&id=2">Product 2</a>
             </div>
         </li>
         <li><a href="index.php?action=userProfile">User Profile</a></li>
-        <li><a href="Controleur/Routeur.php?action=inscription">Inscription</a></li>
+        <li><a href="index.php?action=inscription">Inscription</a></li>
     </ul>
 
 </header>
