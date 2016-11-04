8cd120bf106953a0bb6fc307e529ba5a4827b22a
nsobczak <nicolas.sobczak@isen.yncrea.fr>
2016-11-04 22:20:19 +0100

    Update message erreur + header

--- Vue/header.php
+++ Vue/header.php
@@ -2,6 +2,7 @@
     <a href="index.php?action=accueil"><h1 id="titreSite">GeekProducts.com</h1></a>
 
     <ul>
+<!--        <li><a href="index.php?action=accueil"><img src="Images/Home_Icon.png" title="home"/></a></li>-->
         <li><a href="index.php?action=recherche">Recherche</a></li>
         <li class="dropdown">
             <a href="#" class="dropbtn">Produits</a>
@@ -11,9 +12,11 @@
             </div>
         </li>
         <li><a href="index.php?action=tunnel">Panier</a></li>
+        <li>--</li>
         <li><a href="index.php?action=userProfile">Profil</a></li>
         <li><a href="index.php?action=login">Login</a></li>
         <li><a href="index.php?action=inscription">Inscription</a></li>
+        <li>--</li>
         <li><a href="index.php?action=AdminUser">Admin User</a></li>
         <li><a href="index.php?action=AdminCompteClient">Admin Compte Client</a></li>
 
