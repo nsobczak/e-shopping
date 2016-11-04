57c98315968ae956aa9781fd757719a965ca34b2
nsobczak <nicolas.sobczak@isen.yncrea.fr>
2016-11-04 23:39:57 +0100

    Add work in progress picture in vueErreur

--- Vue/vueErreur.php
+++ Vue/vueErreur.php
@@ -1,7 +1,10 @@
 <section id=erreur>
     <h1> Page d'erreur <h1/>
-
         <p>
             Work in progress
         <p/>
+</section>
+
+<section id=logo>
+    <img class="display" width=30% src="Images/work_progress.jpg" title="work in progress"/>
 </section>
