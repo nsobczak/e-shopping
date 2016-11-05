85840728019fba544ba2d5e93f684f4719e62a8b
nsobczak <nicolas.sobczak@isen.yncrea.fr>
2016-11-05 10:57:44 +0100

    Update vueUserPRofile.php

--- Vue/vueUserProfile.php
+++ Vue/vueUserProfile.php
@@ -5,41 +5,32 @@
 <p>
 <table width="95%" style="line-height: 24px;" border="10">
     <tr>    <!-- table row -->
-        <td colspan="1"><img class="display" width=10% src="Images/profil_utilisateur.jpg" alt="user_picture"
+        <td colspan="6"><img class="display" width=10% src="Images/profil_utilisateur.jpg" alt="user_picture"
                              title="user_picture"/></td>    <!-- table data -->
     </tr>
     <tr>
-        <td> user ID =
-        <td/>
-        <td><?= $userID ?>
-        <td/>
+        <td> user ID :<td/>
+        <td><?= $userID ?><td/>
     </tr>
     <tr>
-        <td> nom =
-        <td/>
-        <td><?= $nom ?>
-        <td/>
+        <td> nom :<td/>
+        <td><?= $nom ?><td/>
     </tr>
     <tr>
-        <td> prénom =
-        <td/>
+        <td> prénom :<td/>
         <td><?= $prenom ?>
         <td/>
     </tr>
     <tr>
-        <td> niveau_accreditation =
-        <td/>
-        <td><?= $niveau_accreditation ?>
-        <td/>
+        <td> niveau_accreditation :<td/>
+        <td><?= $niveau_accreditation ?><td/>
     </tr>
     <tr>
-        <td> mail =
-        <td/>
+        <td> mail :<td/>
         <td><?= $mail ?></td>
     </tr>
     <tr>
-        <td> sha1 du mot de passe =
-        <td/>
+        <td> sha1 du mot de passe :<td/>
         <td><?= $mot_de_passe ?></td>
     </tr>
 </table>
